89eb09981e3d74b5c04d1a86db94d19ebd03e88a
manthan98 <contact@manthanshah.com>
2018-02-19 11:54:59 -0500

    Update port

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 export default {
     // "port": 3005,
     // "mongoUrl": "mongodb://localhost:27017/watsmygrades-api",
-    "port": process.env.PORT,
+    "port": 3005,
     "mongoUrl:": "mongodb://manthan:hello@ds241668.mlab.com:41668/watsmygrade",
     "bodyLimit": "100kb"
 }
