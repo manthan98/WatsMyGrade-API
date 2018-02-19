85ad799e8b9d3e4aa52dc482630915cfcc0aaa70
manthan98 <contact@manthanshah.com>
2018-02-19 11:50:46 -0500

    Add mongodb url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
 export default {
-    "port": 3005,
-    "mongoUrl": "mongodb://localhost:27017/watsmygrades-api",
+    // "port": 3005,
+    // "mongoUrl": "mongodb://localhost:27017/watsmygrades-api",
+    "port": process.env.PORT,
+    "mongoUrl:": "mongodb://manthan:hello@ds241668.mlab.com:41668/watsmygrade",
     "bodyLimit": "100kb"
 }
