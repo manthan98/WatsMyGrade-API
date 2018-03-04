e5f3a00f3acf81ef3afcf67cd917d20c21e96548
manthan98 <contact@manthanshah.com>
2018-03-04 18:07:03 -0500

    heroku

--- src/controller/grade.js
+++ src/controller/grade.js
@@ -32,5 +32,44 @@ export default({ config, db }) => {
         });
     });
 
+    // '/v1/grade/:id' - READ grades for a course.
+    api.get('/:id', (req, res) => {
+        Grade.find({ course: req.params.id }, (err, grades) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(grades);
+        });
+    });
+
+    // '/v1/grade/update/:id' - UPDATE a course.
+    api.put('/update/:id', (req, res) => {
+        Grade.findById(req.params.id, (err, grade) => {
+            if (err) {
+                res.send(err);
+            }
+            grade.name = req.body.name;
+            grade.mark = req.body.mark;
+            grade.weight = req.body.weight;
+            grade.save(err => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json({ message: "Grade information updated." });
+            });
+        });
+    });
+
+    // '/v1/grade/delete/:id' - DELETE a grade.
+    api.delete('/delete/:id', (req, res) => {
+        Grade.findByIdAndRemove(req.params.id, (err, grade) => {
+            let response = {
+                message: "Grade successfully removed.",
+                id: grade._id
+            };
+            res.status(200).send(response);
+        });
+    });
+
     return api;
 }
